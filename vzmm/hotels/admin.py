bee9948b937ef531ef661f7f3238d84b912935ea
Yan Aung <yan.pye.aung@gmail.com>
2013-05-19 15:53:42 -0700

    Add address inline for hotel admin

--- vzmm/hotels/admin.py
+++ vzmm/hotels/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from hotels.models import Hotel, Address
 
-admin.site.register(Hotel)
-admin.site.register(Address)
+from django.forms.models import BaseInlineFormSet
+
+class AddressInline(admin.StackedInline):
+    model = Address
+
+class HotelAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {
+            'fields':['name', 'tag', 'description', 'starting_rate',
+            'comment', 'created_date']
+            }),
+    ]
+    inlines = [AddressInline]
+
+admin.site.register(Hotel, HotelAdmin)
