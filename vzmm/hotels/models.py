b2fdb9df653ef98208965da0632552de1570f7c0
Yan Aung <yan.pye.aung@gmail.com>
2013-05-18 22:27:24 -0700

    Add Hotels model

--- vzmm/hotels/models.py
+++ vzmm/hotels/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Hotel(models.Model):
+    name = models.CharField(max_length=200)
+    tag = models.CharField(max_length=15, blank=True)
+    url = models.URLField(max_length=150, blank=True)
+    description = models.TextField(blank=True)
+    comment = models.CharField(max_length=100, blank=True)
+    starting_rate = models.DecimalField(max_digits=6, decimal_places=2,
+        blank=True)
+    created_date = models.DateTimeField('date created')
+
+
+class Address(models.Model):
+    hotel = models.ForeignKey(Hotel, null=True, blank=True)
+    address = models.CharField(max_length=200)
+    address2 = models.CharField(max_length=200, blank=True)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=25, blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+    phone2 = models.CharField(max_length=20, blank=True)
+    email = models.EmailField(max_length=75, blank=True)
+    email2 = models.EmailField(max_length=75, blank=True)
+
