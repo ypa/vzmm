e9bbdac6e3caad3f9036562dddbc2e6af7d39c69
Yan Aung <yan.pye.aung@gmail.com>
2013-05-27 22:19:27 -0700

    Add routes for hotel index and detail views

---
+++ vzmm/hotels/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, url
+
+from hotels import views
+
+urlpatterns = patterns('',
+    # ex: /hotels/
+    url(r'^$', views.index, name='index'),
+    # ex: /hotels/3/
+    url(r'^(?P<hotel_id>\d+)/$', views.detail, name='detail'),
+)
