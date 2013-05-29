39f0f5295a7bc24a3f79ade696f3d46e4259623b
Yan Aung <yan.pye.aung@gmail.com>
2013-05-29 07:23:23 -0700

    Fix hotel detail template

--- vzmm/static/js/project.js
+++ vzmm/static/js/project.js
@@ -1,1 +1,19 @@
 /* Project specific Javascript goes here. */
+
+//rotating among thumbs and main image
+$(document).ready(function() {
+	$('#thumbs img').click(function(){
+		var main_src = $('#main img').attr('src');
+		$('#main img').attr('src',$(this).attr('src'));
+		$(this).attr('src', main_src);
+	});
+
+	$.getJSON('http://localhost:8080/api/hotels', {
+	  key: 'value',
+	  otherKey: 'otherValue'
+	}, function(data){
+	     // Handles the callback when the data returns
+	     alert(data);
+	});
+
+});
