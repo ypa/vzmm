69a7d964876faf309aff3c31f92598cf6e4ae2e6
Yan Aung <yan.pye.aung@gmail.com>
2013-05-30 22:35:12 -0700

    Add vzmm top level views

---
+++ vzmm/vzmm/views.py
@@ -0,0 +1,16 @@
+
+from hotels.models import Hotel
+from django.shortcuts import render
+
+
+def index(request):
+    latest_hotel_list = Hotel.objects.order_by('-created_date')[:5]
+    classified_hotel_list = Hotel.objects.filter(tag='classified')\
+                            .order_by('-created_date')
+    featured_hotel = Hotel.objects.order_by('-created_date')[0]
+    context = {
+                'latest_hotel_list': latest_hotel_list,
+                'classified_hotel_list': classified_hotel_list,
+                'featured_hotel': featured_hotel
+                }
+    return render(request, 'home.html', context)
