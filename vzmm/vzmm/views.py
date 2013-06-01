916256a1d0dd528495d416ad4cd13275213a6da8
Yan Aung <yan.pye.aung@gmail.com>
2013-05-31 21:53:59 -0700

    Update the home page view

--- vzmm/vzmm/views.py
+++ vzmm/vzmm/views.py
@@ -4,9 +4,9 @@ from django.shortcuts import render
 
 
 def index(request):
-    latest_hotel_list = Hotel.objects.order_by('-created_date')[:5]
+    latest_hotel_list = Hotel.objects.order_by('-created_date')[1:5]
     classified_hotel_list = Hotel.objects.filter(tag='classified')\
-                            .order_by('-created_date')
+                            .order_by('-created_date')[:4]
     featured_hotel = Hotel.objects.order_by('-created_date')[0]
     context = {
                 'latest_hotel_list': latest_hotel_list,
