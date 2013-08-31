8ec1967bcc472a86785a26ce17269d8a8dced07d
Yan Aung <yan.pye.aung@gmail.com>
2013-08-30 17:37:27 -0700

    Add import Cling

--- vzmm/vzmm/wsgi.py
+++ vzmm/vzmm/wsgi.py
@@ -30,6 +30,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vzmm.settings.production")
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 application = Cling(get_wsgi_application())
 
 # Apply WSGI middleware here.
