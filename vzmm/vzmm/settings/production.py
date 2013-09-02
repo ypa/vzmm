80eb9d3175aea5f9215dfd12ea315634aa62a8a5
Yan Aung <yan.pye.aung@gmail.com>
2013-09-01 20:34:40 -0700

    Remove static settings

--- vzmm/vzmm/settings/production.py
+++ vzmm/vzmm/settings/production.py
@@ -85,13 +85,5 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
-# Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
 MEDIA_URL = 'https://s3.amazonaws.com/vzmm-assets/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
