962b1b35bd9848c78149c47bf1b591ac2c1000b3
Yan Aung <yan.pye.aung@gmail.com>
2014-05-01 21:24:16 -0700

    Add a model for email

--- vzmm/api/models.py
+++ vzmm/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Email(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    user_name = models.CharField(max_length=50)
+    user_email = models.EmailField()
+    domain = models.CharField(max_length=50, blank=True)
+    url_path = models.CharField(max_length=150, blank=True)
+    comment = models.TextField()
+
+
+    def __unicode__(self):
+        return self.user_email
