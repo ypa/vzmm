e6cf99f9796df948c5a2bd6e5e491b40203a4ce5
Yan Aung <yan.pye.aung@gmail.com>
2014-05-02 22:02:38 -0700

    Fix api url pattern

--- vzmm/api/urls.py
+++ vzmm/api/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 from api import views
 
 urlpatterns = patterns('',
-    url(r'^email/$', 'views.email_tour'),
+    url(r'^email/$', views.email_tour, name='email'),
 )
